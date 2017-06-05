cdb0fe816840a84714fac5c3352c7e3485f6c806
Hiroshi Miyasato <hiroshi_miyasato@usmp.pe>
2017-06-05 13:41:42 -0500

    PRIMERO COMMIT

---
+++ 2012206149.SLN/201206149.PER/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _201206149.PER
+{
+    public class Class1
+    {
+    }
+}
